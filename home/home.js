84ef123b9042252706c8447548bd33367c034325
Shubhash Singh <subhashsingh2059@gmail.com>
2025-07-30 22:36:07 +0530

    image path changed

--- home/home.js
+++ home/home.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Background Image Slider
     const heroSection = document.querySelector('.hero');
     const backgroundImages = [
-        "../assets/images/students_outside.jpg",
-        '../assets/images/class_from_behind.jpg',
-        '../assets/images/class_students_only.jpg',
-        '../assets/images/lab_with_student.jpg'
+        'assets/images/students_outside.jpg',
+        'assets/images/class_from_behind.jpg',
+        'assets/images/class_students_only.jpg',
+        'assets/images/lab_with_student.jpg'
     ];
     
     let currentImageIndex = 0;
